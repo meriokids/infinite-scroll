7fffd4302bc546f77454c486fc03687ddb96c75a
Mario Alberto Villagómez Pizano <meriokids@gmail.com>
2021-09-27 13:02:11 -0500

    Cambios en JS

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('testing');
+const count = 10;
+const apiKey = 'API-KEY';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    }
+    catch (error) {
+    // Catch error here
+    }
+}
+
+// On Load
+getPhotos();
